1f101be583f0ae4d5bdcb43e7c42441f2b17e8c5
theblackmonk <86014667+theblackmonk@users.noreply.github.com>
2021-12-28 11:24:27 -0500

    Initial commit

---
+++ Opensea.nft.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+
+contract NFTContract is ERC1155, Ownable {
+    
+    uint256 public constant ARTWORK = 0;  //two NFTS
+    uint256 public constant PHOTO = 1;
+
+    constructor() ERC1155("") {
+        _mint(msg.sender, ARTWORK, 1, "");
+        _mint(msg.sender, PHOTO, 2, "");
+    }
+
+    //function that allows us to mint more of these NFTS
+    function mint(address account, uint256 id, uint256 amount) public onlyOwner {
+        _mint(account, id, amount, "");
+    }
+
+    function burn(address account, uint256 id, uint256 amount) public {
+        require(msg.sender == account);
+        _burn(account, id, amount);
+    }
+}
